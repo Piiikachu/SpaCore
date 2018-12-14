0b49b09d3cc0e74452d43b494bed87017b782ea8
Piiikachu <314475440@qq.com>
2018-12-14 17:49:55 +0800

    particle

--- SpaCore/Mixture.cs
+++ SpaCore/Mixture.cs
@@ -1,6 +1,95 @@
-﻿namespace SpaCore
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpaCore
 {
-    internal class Mixture
+    public class Mixture
     {
+        private SPARTA sparta;
+        private string userid;
+
+        public string id;                   // ID of mixture
+        public int nspecies;               // # of species in mixture
+        public List<int> species;               // species[i] = particle species index of 
+                                     //              mixture species I
+         
+        public int ngroup;                 // # of defined groups
+        public List<string> groups;              // group IDs
+        public int[] mix2group;             // m2g[i] = group that mixture species I is in
+         
+         // global attributes
+        public double nrho;                // number density
+        public int nrho_flag;              // 1 if user set nrho
+        public double nrho_user;           // user value
+        public double[] vstream;          // stream velocity
+        public int vstream_flag;           // 1 if user set vstream
+        public double[] vstream_user;     // user value
+        public double temp_thermal;        // thermal temperature
+        public double temp_rot;            // rotational temperature
+        public double temp_vib;            // vibrational temperature
+        public int temp_thermal_flag;      // 1 if user set thermal temp
+        public int temp_rot_flag;          // 1 if user set rotational temp
+        public int temp_vib_flag;          // 1 if user set vibrational temp
+        public double temp_thermal_user;   // user value
+        public double temp_rot_user;       // user value
+        public double temp_vib_user;       // user value
+         
+         // per-species attributes
+        public double[] fraction;           // relative fraction of each species
+        public int[] fraction_flag;         // 1 if user set fraction for a species
+        public double[] fraction_user;      // user fractional value
+         
+         // set by init()
+         
+        public double[] cummulative;        // cummulative fraction for each species
+        public int[] groupsize;             // # of species in each group
+        public int[][] groupspecies;         // list of particle species indices in each group
+        public int[] species2group;         // s2g[i] = group that particle species I is in
+                                     // -1 if species I not in mixture
+        public int[] species2species;       // s2s[i] = mixture species that 
+                                     //   particle species I is
+                                     // -1 if species I not in mixture
+        public double[] vscale;             // pre-computed velocity scale factor
+
+
+        private int all_default , species_default;
+
+        public Mixture(SPARTA sparta, string userid)
+        {
+            this.sparta = sparta;
+            this.userid = userid;
+            vstream = new double[3];
+            vstream_user = new double[3];
+            id = string.Copy(userid);
+            foreach (char c in id)
+            {
+                if (!char.IsLetterOrDigit(c))
+                {
+                    sparta.DumpMessage("Mixture ID must be alphanumeric or underscore characters");
+                }
+            }
+
+            all_default = species_default = 0;
+            switch (id)
+            {
+                case "all":all_default=1;break;
+                case "species":species_default = 1;break;
+                default:
+                    break;
+            }
+            Allocate();
+
+        }
+
+        private void Allocate()
+        {
+            species=
+            throw new NotImplementedException();
+        }
+
+        internal void Command(int narg, string[] arg)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- SpaCore/Particle.cs
+++ SpaCore/Particle.cs
@@ -12,7 +12,7 @@ namespace SpaCore
 
         public class Species
         {                                       // info on each particle species, read from file
-            public char[] id ;    // species ID
+            public char[] id;    // species ID
             public double molwt;           // molecular weight
             public double mass;            // molecular mass
             public double specwt;          // species weight
@@ -40,8 +40,8 @@ namespace SpaCore
 
         public class RotFile
         {
-            public char[] id ;
-            public double[] rottemp ;
+            public char[] id;
+            public double[] rottemp;
             public int ntemp;
             public RotFile()
             {
@@ -68,11 +68,10 @@ namespace SpaCore
 
         public Species species;
         public int nspecies;
-        public int maxvibmod;
+        public int maxvibmode;
 
-        Mixture mixture;
-        int nmixtur;
-        int maxmixture;
+        public List<Mixture> mixture;
+        public int nmixture;
 
         public class OnePart
         {
@@ -113,48 +112,52 @@ namespace SpaCore
 
         public OnePart particles;
 
+        int[] next;                // index of next particle in each grid cell
+
+
         protected int maxgrid;              // max # of indices first can hold
         protected int maxsort;              // max # of particles next can hold
         protected int maxspecies;           // max size of species list
-         
+
         protected FileStream fp;                 // file pointer for species, rotation, vibration
         protected int nfile;                // # of species read from file
         protected int maxfile;              // max size of file list
-         
+
         protected Species filespecies;     // list of species read from file
         protected RotFile filerot;         // list of species rotation info read from file
         protected VibFile filevib;         // list of species vibration info read from file
-         
+
         protected RanPark wrandom;   // RNG for particle weighting
-         
-          // extra custom vectors/arrays for per-particle data
-          // ncustom > 0 if there are any extra arrays
-          // these varaiables are private, others above are public
-          
+
+        // extra custom vectors/arrays for per-particle data
+        // ncustom > 0 if there are any extra arrays
+        // these varaiables are private, others above are public
+
         protected string[] ename;             // name of each attribute
-         
+
         protected int ncustom_ivec;         // # of integer vector attributes
         protected int ncustom_iarray;       // # of integer array attributes
         protected int[] icustom_ivec;        // index into ncustom for each integer vector
         protected int[] icustom_iarray;      // index into ncustom for each integer array
         protected int[] eicol;               // # of columns in each integer array (esize)
-         
+
         protected int ncustom_dvec;         // # of double vector attributes
         protected int ncustom_darray;       // # of double array attributes
         protected int[] icustom_dvec;        // index into ncustom for each double vector
         protected int[] icustom_darray;      // index into ncustom for each double array
         protected int[] edcol;               // # of columns in each double array (esize)
-         
+
         protected int[] custom_restart_flag; // flag on each custom vec/array read from restart
-                                  // used to delete them if not redefined in 
-                                  // restart script
+                                             // used to delete them if not redefined in 
+                                             // restart script
 
 
-
+        private SPARTA sparta;
         public Particle(SPARTA sparta)
         {
+            this.sparta = sparta;
             exist = sorted = false;
-            
+
             particles = null;
 
             nspecies = maxspecies = 0;
@@ -169,31 +172,32 @@ namespace SpaCore
 
             // create two default mixtures
 
-            nmixture = maxmixture = 0;
-            mixture = null;
+
+            mixture = new List<Mixture>();
+            nmixture = 0;
 
             string[] newarg = new string[1];
             newarg[0] = "all";
-            add_mixture(1, newarg);
+            AddMixture(1, newarg);
             newarg[0] = "species";
-            add_mixture(1, newarg);
+            AddMixture(1, newarg);
 
             // custom per-particle vectors/arrays
 
-            ncustom = 0;
+            //ncustom = 0;
             ename = null;
-            etype = esize = ewhich = null;
+            //etype = esize = ewhich = null;
 
             ncustom_ivec = ncustom_iarray = 0;
             icustom_ivec = icustom_iarray = null;
-            eivec = null;
-            eiarray = null;
+            //eivec = null;
+            //eiarray = null;
             eicol = null;
 
             ncustom_dvec = ncustom_darray = 0;
             icustom_dvec = icustom_darray = null;
-            edvec = null;
-            edarray = null;
+            //edvec = null;
+            //edarray = null;
             edcol = null;
 
             custom_restart_flag = null;
@@ -202,10 +206,36 @@ namespace SpaCore
 
             wrandom = null;
 
-            copy = copymode = 0;
+            //copy = copymode = 0;
         }
 
+        private void AddMixture(int narg, string[] arg)
+        {
+            if (narg < 1) sparta.DumpMessage("Illegal mixture command");
 
+            // imix = index if mixture ID already exists
+            // else instantiate a new mixture
 
+            int imix = FindMixture(arg[0]);
+
+            if (imix < 0)
+            {
+
+                imix = nmixture;
+                nmixture++;
+                mixture.Add(new Mixture(sparta, arg[0]));
+            }
+
+            mixture[imix].Command(narg, arg);
+        }
+
+        private int FindMixture(string id)
+        {
+            
+            
+            for (int i = 0; i < nmixture; i++)
+                if (string.Equals(id, mixture[i].id)) return i;
+            return -1;
+        }
     }
 }
